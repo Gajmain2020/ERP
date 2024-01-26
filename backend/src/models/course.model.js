fd368b7a820f0d26931e69da8114b86993629763
Gajendra sahu <82883877+Gajmain2020@users.noreply.github.com>
2024-01-26 17:15:09 +0530

    Timestamps added to models

--- backend/src/models/course.model.js
+++ backend/src/models/course.model.js
@@ -2,18 +2,21 @@ import mongoose from "mongoose";
 
 const reqString = { type: String, required: true };
 
-const courseSchema = new mongoose.Schema({
-  courseCode: reqString,
-  courseName: reqString,
-  courseShortName: reqString,
-  semester: reqString,
-  department: reqString,
-  courseType: {
-    type: String,
-    enum: ["core", "elective", "open"],
-    default: "core",
+const courseSchema = new mongoose.Schema(
+  {
+    courseCode: reqString,
+    courseName: reqString,
+    courseShortName: reqString,
+    semester: reqString,
+    department: reqString,
+    courseType: {
+      type: String,
+      enum: ["core", "elective", "open"],
+      default: "core",
+    },
+    takenBy: [{ teacherName: String, teacherId: String }],
   },
-  takenBy: [{ teacherName: String, teacherId: String }],
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Course", courseSchema);
